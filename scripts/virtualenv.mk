282883c2d44855f8149e6057f37158b2fbdcc66e
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-12-09 10:32:09 -0800

    Cache all requirements.txt in pip-cache
    Download all Python package dependencies for all platforms in pip-cache
    so that users of Freedom E SDK can work offline.
    
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- scripts/virtualenv.mk
+++ scripts/virtualenv.mk
@@ -1,8 +1,16 @@
 
-venv/bin/activate: requirements.txt
+pip-cache: requirements.txt
 	python3 -m venv venv
-	. $@ && pip install --upgrade pip
-	. $@ && pip install -r $<
+	. venv/bin/activate && pip install --upgrade pip
+	. venv/bin/activate && pip install pip-download
+	mkdir -p $@
+	. venv/bin/activate && pip-download -i https://pypi.org/simple -d $@ -r $<
+	rm -rf venv/
+
+venv/bin/activate: requirements.txt pip-cache
+	python3 -m venv venv
+	. $@ && pip install --no-index --find-links pip-cache --upgrade pip
+	. $@ && pip install --no-index --find-links pip-cache -r $<
 
 .PHONY: clean-virtualenv
 clean-virtualenv:
